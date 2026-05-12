1779506f99d08a8d6018e0ce7aae7c2c9fef0834
Muhammad Naveed <naveed5333425@gmail.com>
2026-05-13 01:57:52 +0500

    first commit

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,16 +1,11 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
-import {
-  syncUserCreation,
-  syncUserDeletion,
-  syncUserUpdation,
-} from "@/config/inngest/functions";
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 export const { GET, POST } = serve({
   client: inngest,
   functions: [
     syncUserCreation,
-    syncUserDeletion,
     syncUserUpdation,
+    syncUserDeletion,
   ],
 });
